c8c9e1f9f189c40d155790ae1ff031ce9d9151d1
Michael R. Davis <mrdvt@cpan.org>
2007-04-15 00:46:57 +0000

    Import of MRDVT/Data-Convert-MicrochipTechnology-Float-0.02 from CPAN.
    gitpan-cpan-distribution: Data-Convert-MicrochipTechnology-Float
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         MRDVT/Data-Convert-MicrochipTechnology-Float-0.02.tar.gz
    gitpan-cpan-author:       MRDVT
    gitpan-cpan-maturity:     released

--- lib/Data/Convert/MicrochipTechnology/Float.pm
+++ lib/Data/Convert/MicrochipTechnology/Float.pm
@@ -3,29 +3,29 @@ use strict;
 
 BEGIN {
     use vars qw($VERSION);
-    $VERSION     = '0.01';
+    $VERSION     = '0.02';
 }
 
 =head1 NAME
 
-Data::Convert::MicrochipTechnology::Float - Converts Microchip Technology 32 bit float to a number
+Data::Convert::MicrochipTechnology::Float - Converts Microchip Technology 32-bit float to a number
 
 =head1 SYNOPSIS
 
   use Data::Convert::MicrochipTechnology::Float;
   my $object = Data::Convert::MicrochipTechnology::Float->new();
   my $float=$obj->convert("\0\0\0\0");
-  print "$float\n";
+  print "Float: $float\n";
 
 =head1 DESCRIPTION
 
-The format of the PIC 32-bit float is eeeeeeee smmmmmmm mmmmmmmm mmmmmmmm
+The format of the PIC 32-bit float is eeeeeeee smmmmmmm mmmmmmmm mmmmmmmm (4-bytes => 8-bit biased exponent, 1-bit sign, 23-bit significand)
 
-  The number has value v: v = s × 2e × m
+  The number has value v: v = s * 2**e * m
   s = +1 (positive numbers) when the sign bit is 0
   s = -1 (negative numbers) when the sign bit is 1
   e = Exp - 127 (the exponent is biased with 127)
-  m = 1.fraction in binary (that is, the significand is the binary number 1 followed by the radix point followed by the binary bits of the fraction). Therefore, 1 = m < 2.
+  m = 1.fraction in binary (the significand is the binary number 1 followed by the radix point followed by the binary bits of the fraction). Therefore, 1 = m < 2.
 
 =head1 USAGE
 
@@ -52,22 +52,24 @@ sub new {
 
   my $float=$obj->convert("\0\0\0\0");
   my @list=$obj->convert("\0\0\0\0", "\0\0\0\0");
+  my $listref=$obj->convert("\0\0\0\0", "\0\0\0\0");
+  my $float=$obj->convert([0,0,0,0]);
+  my @list=$obj->convert("\0\0\0\0", [0,0,0,0]);
+  my $listref=$obj->convert("\0\0\0\0", [0,0,0,0]);
 
 =cut
 
 sub convert {
   my $self=shift();
-  if (1 == scalar(@_)) {
-    return $self->float_from_string(shift());
-  } else {
-    my @list=map {$self->float_from_string($_)} @_;
-    return wantarray ? @list : \@list;
-  }
+  my @list=map {ref($_) eq 'ARRAY' ?
+                  $self->float_from_array(@$_) :
+                  $self->float_from_string($_)} @_;
+  return scalar(@list) == 1 ? $list[0] : (wantarray ? @list : \@list);
 }
 
 =head2 float_from_string
 
-  my $float=$obj->float_from_array("\0\0\0\0");
+  my $float=$obj->float_from_string("\0\0\0\0");
 
 =cut
 
@@ -79,19 +81,24 @@ sub float_from_string {
 
 =head2 float_from_array
 
-  my $float=$obj->float_from_array("\0", "\0", "\0", "\0");
+  my $float=$obj->float_from_array(0, 0, 0, 0);
 
 =cut
 
 sub float_from_array {
   my $self=shift();
   my ($b0, $b1, $b2, $b3)=@_;
-  my $s = $b1 & 128 ? -1 : 1;
-  my $e = $b0 - 127;
-  my $m = 1 + ((($b1 & 127) * 256 + $b2) * 256 + $b3) / (2 ** 23);
-  my $v = $s * 2 ** $e * $m;
-  $v = 0 if (0==$b0 and (0==$b1 or 128==$b1) and 0==$b2 and 0==$b3); #eliminates the round off error
-  return $v;
+  if (0==$b0 and (0==$b1 or 128==$b1) and 0==$b2 and 0==$b3) {
+    #eliminates rounding errors for +/- zero
+    return 0;
+  } else {
+    my $s = $b1 & 128 ? -1 : 1;
+    my $e = $b0 - 127;
+    #Should 2**23 be 2**23 - 1 ?
+    my $m = 1 + ((($b1 & 127) * 256 + $b2) * 256 + $b3) / (2 ** 23);
+    my $v = $s * 2 ** $e * $m;
+    return $v;
+  }
 }
 
 =head1 BUGS
@@ -100,7 +107,7 @@ The math introduces floating point rounding errors.
 
 =head1 TODO
 
-Add a strict bit vector capability to eliminate any rounding errors.
+Add a bit vector capability to eliminate any rounding errors.
 
 =head1 SUPPORT
 
